b9591caa6287cefcbc939a8f6f824f568a9d368a
Sean Kelly <invertedrdm@gmail.com>
2021-03-25 16:43:20 -0400

    added terminal prompts and test writefile

---
+++ index.js
@@ -0,0 +1,57 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'uTitle',
+      message: 'What is the title of the project?',
+    },
+    {
+        type: 'input',
+        name: 'uDescription',
+        message: 'Describe your application',
+    },
+    {
+        type: 'input',
+        name: 'uInstall',
+        message: 'How does a user install your app?',
+    },
+    {
+        type: 'input',
+        name: 'uUsage',
+        message: 'How does a user user your app?',
+    },
+    {
+        type: 'input',
+        name: 'uContribute',
+        message: 'How does somebody contribute to the development of your app?',
+    },
+    {
+        type: 'input',
+        name: 'uTests',
+        message: 'Provide testing examples for your application',
+    },
+    {
+        type: 'input',
+        name: 'uGithub',
+        message: 'What is your github username?',
+    },
+    {
+        type: 'input',
+        name: 'uEmail',
+        message: 'What is your email address?',
+    },
+    {
+      type: 'list',
+      message: 'Which license does your application use?',
+      name: 'uLicense',
+      choices: ['MIT', 'GNU'],
+    },
+  ])
+  .then((data) => {
+    fs.writeFile('SAMPLE.md', JSON.stringify(data, null, '\t'), (err) =>
+      err ? console.log(err) : console.log('Success!')
+    );
+  });
